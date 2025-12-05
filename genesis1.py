cfd17e7e9e772db80b0f6eefa13c9c18d33a678c
JASON MA <raiden2001@github>
2025-12-05 11:52:57 -0500

    fresh clean start

---
+++ genesis1.py
@@ -0,0 +1,43 @@
+#Parallel Simulation
+# create n_evns scene
+
+import genesis as gs 
+import torch
+################ init ##########################
+gs.init(backend=gs.gpu)
+############################## create a scene #####################
+scene = gs.scene(
+    show_viewer = True,
+    viewer_options = gs.options.ViewerOptions(
+        camera_pos = (3.5,-1.0,2.5),
+        camera_lookat = (0.0,0.0,0.5),
+        camera_fov = 40,
+    ),
+    rigid_options = gs.options.RigidOptions(
+        dt                  = 0.01,            
+    ),
+                 
+)
+
+###########################entities##############################
+plane = scene.add_entity(
+    gs.morphs.Plane(),
+)
+
+franka = scene.add_entity(
+    gs.morphs.MJCF(file = 'xml/franka_emika_panda/panda.xml'),
+)
+################################ build ###########################
+# create 20 parallel environments
+B=20
+scene.build(n_envs=B,env_spacing=(1.0,1.0))
+
+#control all robots
+franka.control_dofs_postion(
+        torch.tile(
+            torch.tensor([0,0,0,-1.0,0,1.0,0,0.02,0.02],device=gs.device),(3000,1)
+    ),
+)
+
+for i in range(1000):
+    scene.step()
