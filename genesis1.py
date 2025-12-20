56b43a671de56161884c32f8cfdcec72d120e99e
JASON MA <raiden2001@github>
2025-12-20 02:37:56 -0500

    update genesis1

--- genesis1.py
+++ genesis1.py
@@ -186,3 +186,48 @@ for i in range(1000):
     scene.step()
 
     #test
+    # --- (scene and robot creation omitted, identical to the sections above) ---
+
+# Retrieve some commonly used handles
+rigid        = scene.sim.rigid_solver          # low-level rigid body solver
+end_effector = franka.get_link("hand")        # Franka gripper frame
+cube_link    = cube.get_link("box_baselink")   # the link we want to pick
+
+################ Reach pre-grasp pose ################
+q_pregrasp = franka.inverse_kinematics(
+    link = end_effector,
+    pos  = np.array([0.65, 0.0, 0.13]),  # just above the cube
+    quat = np.array([0, 1, 0, 0]),        # down-facing orientation
+)
+franka.control_dofs_position(q_pregrasp[:-2], np.arange(7))  # arm joints only
+for _ in range(50):
+    scene.step()
+
+################ Attach (activate suction) ################
+link_cube   = np.array([cube_link.idx],   dtype=gs.np_int)
+link_franka = np.array([end_effector.idx], dtype=gs.np_int)
+rigid.add_weld_constraint(link_cube, link_franka)
+
+################ Lift and transport ################
+q_lift = franka.inverse_kinematics(
+    link = end_effector,
+    pos  = np.array([0.65, 0.0, 0.28]),  # lift up
+    quat = np.array([0, 1, 0, 0]),
+)
+franka.control_dofs_position(q_lift[:-2], np.arange(7))
+for _ in range(50):
+    scene.step()
+
+q_place = franka.inverse_kinematics(
+    link = end_effector,
+    pos  = np.array([0.4, 0.2, 0.18]),  # target place pose
+    quat = np.array([0, 1, 0, 0]),
+)
+franka.control_dofs_position(q_place[:-2], np.arange(7))
+for _ in range(100):
+    scene.step()
+
+################ Detach (release suction) ################
+rigid.delete_weld_constraint(link_cube, link_franka)
+for _ in range(400):
+    scene.step()
