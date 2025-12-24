1551a1cff0902165185972cf97b86ae4374a0c13
JASON MA <raiden2001@github>
2025-12-24 13:22:08 -0500

    update genesis1.py

--- genesis1.py
+++ genesis1.py
@@ -62,6 +62,10 @@ scene.build()
 #rgb,depth, segmentation,normal = cam.render(rgb=True,depth=True,segmentation=True,normal=True)
 cam.start_recording()
 
+motors_dof = np.arange(7)
+fingers_dof = np.arange(7,9)
+
+
 
 for i in range(120):
     scene.step()
@@ -231,3 +235,31 @@ for _ in range(100):
 rigid.delete_weld_constraint(link_cube, link_franka)
 for _ in range(400):
     scene.step()
+
+################## IK and MOTION#####################################3
+
+#get the end-effector link
+end_effector = franka.get_link('hand')
+
+#move to pre-grasp pose
+qpos = franka.inverse_kinematics(
+    link = end_effector,
+    pos = np.array([0.65,0.0,0.25]),
+    quat = np.array([0,1,0,0]),
+)
+#gripper open pos
+qpos[-2:] = 0.04
+path = franka.plan_path(
+    qpos_goal = qpos,
+    num_waypoints = 200, #2s duration sets
+)
+#Execute the planned path
+for waypoint in path:
+    franka.control_dofs_position(waypoint)
+    scene.step() #finalize the scene
+
+#allow robot to reach last waypoint
+for i in range(100):
+    scene.step()
+
+    
